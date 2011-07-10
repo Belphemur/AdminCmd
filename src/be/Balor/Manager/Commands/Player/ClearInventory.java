6e1b48d1e1cad75f39ecadf657a4000d2f9a4df6
Antoine Aflalo <antoineaf@gmail.com>
2011-07-10 20:39:50 +0200

    added Clear inventory, memory and setTime

---
+++ src/be/Balor/Manager/Commands/Player/ClearInventory.java
@@ -0,0 +1,73 @@
+/************************************************************************
+ * This file is part of AdminCmd.									
+ *																		
+ * AdminCmd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by	
+ * the Free Software Foundation, either version 3 of the License, or		
+ * (at your option) any later version.									
+ *																		
+ * AdminCmd is distributed in the hope that it will be useful,	
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of		
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
+ * GNU General Public License for more details.							
+ *																		
+ * You should have received a copy of the GNU General Public License
+ * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+package be.Balor.Manager.Commands.Player;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.Balor.bukkit.AdminCmd.AdminCmdWorker;
+
+import be.Balor.Manager.ACCommands;
+
+/**
+ * @author Balor (aka Antoine Aflalo)
+ * 
+ */
+public class ClearInventory extends ACCommands {
+
+	/**
+	 * 
+	 */
+	public ClearInventory() {
+		permNode = "admincmd.player.clear";
+		cmdName = "bal_pclear";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * be.Balor.Manager.ACCommands#execute(org.bukkit.command.CommandSender,
+	 * java.lang.String[])
+	 */
+	@Override
+	public void execute(CommandSender sender, String... args) {
+		Player target = AdminCmdWorker.getInstance().getUser(args, permNode);
+		if (target == null)
+			return;
+		target.getInventory().clear();
+		target.getInventory().setHelmet(null);
+		target.getInventory().setChestplate(null);
+		target.getInventory().setLeggings(null);
+		target.getInventory().setBoots(null);
+		sender.sendMessage(ChatColor.RED + "Inventory of " + ChatColor.WHITE + target.getName()
+				+ ChatColor.RED + " cleared");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see be.Balor.Manager.ACCommands#argsCheck(java.lang.String[])
+	 */
+	@Override
+	public boolean argsCheck(String... args) {
+		// TODO Auto-generated method stub
+		return args != null;
+	}
+
+}
---
+++ src/be/Balor/Manager/Commands/Server/Memory.java
@@ -0,0 +1,71 @@
+/************************************************************************
+ * This file is part of AdminCmd.									
+ *																		
+ * AdminCmd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by	
+ * the Free Software Foundation, either version 3 of the License, or		
+ * (at your option) any later version.									
+ *																		
+ * AdminCmd is distributed in the hope that it will be useful,	
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of		
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
+ * GNU General Public License for more details.							
+ *																		
+ * You should have received a copy of the GNU General Public License
+ * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+package be.Balor.Manager.Commands.Server;
+
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+
+import be.Balor.Manager.ACCommands;
+
+/**
+ * @author Balor (aka Antoine Aflalo)
+ * 
+ */
+public class Memory extends ACCommands {
+
+	/**
+	 * 
+	 */
+	public Memory() {
+		permNode = "admincmd.server.memory";
+		cmdName = "bal_memory";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * be.Balor.Manager.ACCommands#execute(org.bukkit.command.CommandSender,
+	 * java.lang.String[])
+	 */
+	@Override
+	public void execute(CommandSender sender, String... args) {
+		long usedMB = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024L / 1024L;
+		sender.sendMessage(ChatColor.GOLD + "Max Memory : " + ChatColor.WHITE
+				+ Runtime.getRuntime().maxMemory() / 1024L / 1024L + "MB");
+		sender.sendMessage(ChatColor.DARK_RED + "Used Memory : " + ChatColor.WHITE + usedMB + "MB");
+		sender.sendMessage(ChatColor.DARK_GREEN + "Free Memory : " + ChatColor.WHITE
+				+ Runtime.getRuntime().freeMemory() / 1024L / 1024L + "MB");
+		for (World w : sender.getServer().getWorlds()) {
+			sender.sendMessage(w.getEnvironment() + " \"" + w.getName() + "\": "
+					+ w.getLoadedChunks().length + " chunks, " + w.getEntities().size()
+					+ " entities");
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see be.Balor.Manager.ACCommands#argsCheck(java.lang.String[])
+	 */
+	@Override
+	public boolean argsCheck(String... args) {
+		return true;
+	}
+
+}
