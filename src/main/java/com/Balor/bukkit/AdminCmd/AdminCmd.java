f474ab138a60aab93b0162880e56c43d1d244f8c
Antoine Aflalo <antoineaf@gmail.com>
2011-07-28 11:34:54 +0200

    locale key error

--- src/main/java/com/Balor/bukkit/AdminCmd/AdminCmd.java
+++ src/main/java/com/Balor/bukkit/AdminCmd/AdminCmd.java
@@ -212,7 +212,7 @@ public class AdminCmd extends JavaPlugin {
 		Utils.addLocale("unban", ChatColor.YELLOW + "%player is now unbanned.");
 		Utils.addLocale("killMob", ChatColor.RED + "Killing mobs (" + ChatColor.WHITE + "%type"
 				+ ChatColor.RED + ") of worlds : " + ChatColor.DARK_PURPLE + "%worlds");
-		Utils.addLocale("killedMob", "%nbKilled" + ChatColor.DARK_RED + " mobs have been killed.");
+		Utils.addLocale("killedMobs", "%nbKilled" + ChatColor.DARK_RED + " mobs have been killed.");
 
 		LocaleManager.getInstance().save();
 		LocaleManager.getInstance().load();
