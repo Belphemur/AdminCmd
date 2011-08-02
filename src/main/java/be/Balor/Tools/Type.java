f157b84db811ba3b90ab69c6f8c3a0e44668ea29
Antoine Aflalo <antoineaf@gmail.com>
2011-08-02 16:53:42 +0200

    revert

--- src/main/java/be/Balor/Tools/Type.java
+++ src/main/java/be/Balor/Tools/Type.java
@@ -21,7 +21,7 @@ package be.Balor.Tools;
  * 
  */
 public enum Type {
-	FLY, VULCAN, GOD, THOR, BANNED, FIREBALL, SPYMSG, FREEZED, MUTED, MOB_LIMIT, NO_PICKUP, WEATHER_FREEZED, AFK;
+	FLY, VULCAN, GOD, THOR, BANNED, FIREBALL, SPYMSG, FREEZED, MUTED, MOB_LIMIT, NO_PICKUP, WEATHER_FREEZED;
 	@Override
 	public String toString() {
 		String s = super.toString();
