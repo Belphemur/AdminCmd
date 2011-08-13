ee06d57c928646d52528e40d9f097b0340eeed6b
Antoine Aflalo <antoineaf@gmail.com>
2011-08-13 16:40:02 +0200

    little bug with the number of kits

--- src/main/java/be/Balor/Tools/FilesManager.java
+++ src/main/java/be/Balor/Tools/FilesManager.java
@@ -380,7 +380,7 @@ public class FilesManager {
 				result.put(key, new ArrayList<MaterialContainer>(items));
 				items.clear();
 			}
-			System.out.print("[AdminCmd] "+items.size()+" kits loaded.");
+			System.out.print("[AdminCmd] "+result.size()+" kits loaded.");
 		}
 		return result;
 	}
