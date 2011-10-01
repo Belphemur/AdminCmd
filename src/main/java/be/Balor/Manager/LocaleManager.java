0eaf2c2f42720dc66c2c7ad3bfaa004bf36b4ef7
Antoine Aflalo <antoineaf@gmail.com>
2011-10-01 11:49:10 +0200

    optimized localeManager using regex.

--- src/main/java/be/Balor/Manager/LocaleManager.java
+++ src/main/java/be/Balor/Manager/LocaleManager.java
@@ -16,13 +16,13 @@
  ************************************************************************/
 package be.Balor.Manager;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-import be.Balor.Tools.ACLogger;
 import be.Balor.Tools.Configuration.ExtendedConfiguration;
 
 /**
@@ -104,29 +104,30 @@ public class LocaleManager {
 			return null;
 		String locale = localeFile.getString(key);
 		if (locale != null && values != null)
-			for (String toReplace : values.keySet()) {
-				locale = recursiveLocale(locale);
-				try {
-					locale = locale.replaceAll("%" + toReplace, values.get(toReplace));
-				} catch (StringIndexOutOfBoundsException e) {
-					locale = locale.replaceAll("%" + toReplace,
-							values.get(toReplace).replaceAll("\\W", ""));
-				}
-
-			}
+			locale = recursiveReplaceLocale(locale, values);
 		return locale;
 	}
 
-	private String recursiveLocale(String locale) {
+	private String recursiveReplaceLocale(String locale, Map<String, String> values) {
 		String ResultString = null;
 		String result = locale;
 		try {
 			Pattern regex = Pattern.compile("#([\\w]+)#");
 			Matcher regexMatcher = regex.matcher(locale);
-			if (regexMatcher.find()) {
+			while (regexMatcher.find()) {
 				ResultString = regexMatcher.group(1);
-				ACLogger.info(ResultString);
 				String recLocale = localeFile.getString(ResultString);
+				if (recLocale != null)
+					result = regexMatcher.replaceFirst(recLocale);
+				else
+					result = regexMatcher.replaceFirst("");
+				regexMatcher = regex.matcher(result);
+			}
+			regex = Pattern.compile("%([\\w]+)");
+			regexMatcher = regex.matcher(result);
+			while (regexMatcher.find()) {
+				ResultString = regexMatcher.group(1);
+				String recLocale = values.get(ResultString);
 				if (recLocale != null)
 					result = regexMatcher.replaceFirst(recLocale);
 				else
@@ -144,13 +145,9 @@ public class LocaleManager {
 			return null;
 		String locale = localeFile.getString(key);
 		if (locale != null && alias != null) {
-			locale = recursiveLocale(locale);
-
-			try {
-				locale = locale.replaceAll("%" + alias, replaceBy);
-			} catch (StringIndexOutOfBoundsException e) {
-				locale = locale.replaceAll("%" + alias, replaceBy.replaceAll("\\W", ""));
-			}
+			HashMap<String, String> replace = new HashMap<String, String>();
+			replace.put(alias, replaceBy);
+			locale = recursiveReplaceLocale(locale, replace);
 		}
 
 		return locale;
