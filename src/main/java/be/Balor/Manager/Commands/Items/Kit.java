bbc6eedecacb01366d8a242c216b9ee62ef4048e
Antoine Aflalo <antoineaf@gmail.com>
2011-10-01 12:56:43 +0200

    typo correction (last one --)

--- src/main/java/be/Balor/Manager/Commands/Items/Kit.java
+++ src/main/java/be/Balor/Manager/Commands/Items/Kit.java
@@ -84,7 +84,7 @@ public class Kit extends CoreCommand {
 			return;
 		}
 		ACPlayer actarget = ACPlayer.getPlayer(target);
-		if (!PermissionManager.hasPerm(sender, "admincmd.items.nodelay", false)) {
+		if (!PermissionManager.hasPerm(sender, "admincmd.item.nodelay", false)) {
 			long nextuse = actarget.getLastKitUse(kit.getName()) + kit.getDelay() * 1000;
 			long now = System.currentTimeMillis();
 			if (now < nextuse) {
@@ -135,7 +135,7 @@ public class Kit extends CoreCommand {
 	 */
 	@Override
 	public void registerBukkitPerm() {
-		plugin.getPermissionLinker().addPermChild("admincmd.items.nodelay");
+		plugin.getPermissionLinker().addPermChild("admincmd.item.nodelay", bukkitDefault);
 		super.registerBukkitPerm();
 	}
 
