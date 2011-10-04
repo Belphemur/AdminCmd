36f96f599ef65896357aab517bc10184092e243d
Lathanael <101@abwesend.de>
2011-10-04 17:18:36 +0200

    Changed way of the Respawn handling. Now more flexible.

--- src/main/java/be/Balor/Listeners/ACPlayerListener.java
+++ src/main/java/be/Balor/Listeners/ACPlayerListener.java
@@ -33,6 +33,7 @@ import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.util.Vector;
 
 import be.Balor.Manager.CommandManager;
@@ -52,7 +53,7 @@ import belgium.Balor.Workers.InvisibleWorker;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
 public class ACPlayerListener extends PlayerListener {
 	@Override
@@ -84,7 +85,7 @@ public class ACPlayerListener extends PlayerListener {
 			// event.setCancelled(true);
 			/**
 			 * https://github.com/Bukkit/CraftBukkit/pull/434
-			 * 
+			 *
 			 * @author Evenprime
 			 */
 			((CraftPlayer) p).getHandle().netServerHandler.teleport(event.getFrom());
@@ -159,10 +160,21 @@ public class ACPlayerListener extends PlayerListener {
 	public void onPlayerRespawn(PlayerRespawnEvent event) {
 		Player player = event.getPlayer();
 		playerRespawnOrJoin(player);
-		if (ACHelper.getInstance().getConfBoolean("respawnAtSpawnPoint")) {
-			Location loc = null;
-			String worldName = player.getWorld().getName();
+		String spawn = ACHelper.getInstance().getConfString("globalRespawnSetting");
+		Location loc = null;
+		String worldName = player.getWorld().getName();
+		if (spawn.equalsIgnoreCase("") || spawn.equalsIgnoreCase("spawn")) {
 			loc = ACWorld.getWorld(worldName).getSpawn();
+			if (loc == null)
+				loc = player.getWorld().getSpawnLocation();
+			event.setRespawnLocation(loc);
+		} else if (spawn.equalsIgnoreCase("home")) {
+			loc = ACPlayer.getPlayer(player).getHome(worldName);
+			if (loc == null)
+				loc = player.getWorld().getSpawnLocation();
+			event.setRespawnLocation(loc);
+		} else if (spawn.equalsIgnoreCase("bed")) {
+			loc = player.getBedSpawnLocation();
 			if (loc == null)
 				loc = player.getWorld().getSpawnLocation();
 			event.setRespawnLocation(loc);
@@ -278,7 +290,7 @@ public class ACPlayerListener extends PlayerListener {
 
 	/**
 	 * Tp at see mode
-	 * 
+	 *
 	 * @param p
 	 */
 	private void tpAtSee(ACPlayer player) {
