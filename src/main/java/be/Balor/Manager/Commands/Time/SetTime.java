9954f86532c1bb0d31064586f6068a5a7ea72e94
Lathanael <101@abwesend.de>
2011-10-10 15:58:36 +0200

    Added a new permission to enable users the independent use of /time pause/unpause and the other time commands

--- src/main/java/be/Balor/Manager/Commands/Time/SetTime.java
+++ src/main/java/be/Balor/Manager/Commands/Time/SetTime.java
@@ -1,16 +1,16 @@
 /************************************************************************
- * This file is part of AdminCmd.									
- *																		
+ * This file is part of AdminCmd.
+ *
  * AdminCmd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * AdminCmd is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdminCmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
@@ -21,16 +21,17 @@ import org.bukkit.command.CommandSender;
 
 import be.Balor.Manager.Commands.CommandArgs;
 import be.Balor.Manager.Commands.CoreCommand;
+import be.Balor.Manager.Permissions.PermissionManager;
 import be.Balor.Tools.Utils;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
 public class SetTime extends CoreCommand {
 
 	/**
-	 * 
+	 *
 	 */
 	public SetTime() {
 		permNode = "admincmd.time.set";
@@ -39,19 +40,24 @@ public class SetTime extends CoreCommand {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * be.Balor.Manager.ACCommands#execute(org.bukkit.command.CommandSender,
 	 * java.lang.String[])
 	 */
 	@Override
 	public void execute(CommandSender sender, CommandArgs args) {
+		if ((args.getString(0).equalsIgnoreCase("pause")
+				|| args.getString(0).equalsIgnoreCase("unpause"))
+				&& !(PermissionManager.hasPerm(sender, "admincmd.time.pause")))
+			return;
+
 		Utils.timeSet(sender, args.getString(0));
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see be.Balor.Manager.ACCommands#argsCheck(java.lang.String[])
 	 */
 	@Override
@@ -59,4 +65,12 @@ public class SetTime extends CoreCommand {
 		return args != null && args.length >= 1;
 	}
 
+	/* (non-Javadoc)
+	 * @see be.Balor.Manager.Commands.CoreCommand#registerBukkitPerm()
+	 */
+	@Override
+	public void registerBukkitPerm() {
+		plugin.getPermissionLinker().addPermChild("admincmd.time.pause");
+		super.registerBukkitPerm();
+	}
 }
