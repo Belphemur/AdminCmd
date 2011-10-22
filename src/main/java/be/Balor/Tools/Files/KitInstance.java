2332e188a5461c0f35b695cfd0e06e58ac69500d
Antoine Aflalo <antoineaf@gmail.com>
2011-10-23 01:21:28 +0200

    should resolve problem with Permissions

--- src/main/java/be/Balor/Tools/Files/KitInstance.java
+++ src/main/java/be/Balor/Tools/Files/KitInstance.java
@@ -1,6 +1,7 @@
 package be.Balor.Tools.Files;
 
 import be.Balor.Tools.MaterialContainer;
+import be.Balor.bukkit.AdminCmd.ACPluginManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,23 +9,25 @@ import org.bukkit.inventory.ItemStack;
 
 public class KitInstance {
 
-	private String name = null;
+	private final String name;
 	private int delay = 0;
-	private List<MaterialContainer> items = null;
+	private final List<MaterialContainer> items;
 
 	public KitInstance(String name, int delay, List<MaterialContainer> items) {
 		this.name = name;
 		this.delay = delay;
 		this.items = items;
+		ACPluginManager.getPluginInstance("Core").getPermissionLinker()
+				.addOnTheFly("admincmd.kit." + name, "admincmd.kit.*");
 	}
 
 	/**
 	 * Get ItemStacks for given kit
-	 *
+	 * 
 	 * @param kit
 	 * @return
 	 */
-	public ArrayList<ItemStack> getItemStacks() {
+	public List<ItemStack> getItemStacks() {
 		ArrayList<ItemStack> result = new ArrayList<ItemStack>();
 		try {
 			// return Utils.oddItem.getItemGroup(kit, -1));
@@ -34,9 +37,11 @@ public class KitInstance {
 			result.add(mc.getItemStack());
 		return result;
 	}
+
 	public int getDelay() {
 		return delay;
 	}
+
 	/**
 	 * @return the name
 	 */
