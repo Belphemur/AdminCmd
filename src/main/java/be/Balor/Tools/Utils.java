a80c8d3b877219507eb90e9985d89ed34b6fb23d
Lathanael <101@abwesend.de>
2011-11-27 13:09:33 +0100

    Fixed prefixes appearing twice (hopefully) if using mChat

--- src/main/java/be/Balor/Tools/Utils.java
+++ src/main/java/be/Balor/Tools/Utils.java
@@ -68,7 +68,7 @@ import belgium.Balor.Workers.InvisibleWorker;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
 public class Utils {
 	public static OddItemBase oddItem = null;
@@ -83,12 +83,12 @@ public class Utils {
 
 	/**
 	 * @author Balor (aka Antoine Aflalo)
-	 * 
+	 *
 	 */
 
 	/**
 	 * Translate the id or name to a material
-	 * 
+	 *
 	 * @param mat
 	 * @return Material
 	 */
@@ -120,7 +120,7 @@ public class Utils {
 
 	/**
 	 * Parse a string and replace the color in it
-	 * 
+	 *
 	 * @author Speedy64
 	 * @param toParse
 	 * @return
@@ -163,7 +163,7 @@ public class Utils {
 
 	/**
 	 * Check if the command sender is a Player
-	 * 
+	 *
 	 * @return
 	 */
 	public static boolean isPlayer(CommandSender sender) {
@@ -182,7 +182,7 @@ public class Utils {
 
 	/**
 	 * Heal or refill the FoodBar of the selected player.
-	 * 
+	 *
 	 * @param name
 	 * @return
 	 */
@@ -221,7 +221,7 @@ public class Utils {
 
 	/**
 	 * Get the complete player name with all prefix
-	 * 
+	 *
 	 * @param player
 	 *            player to get the name
 	 * @param sender
@@ -247,7 +247,7 @@ public class Utils {
 
 	/**
 	 * Get the user and check who launched the command.
-	 * 
+	 *
 	 * @param sender
 	 * @param args
 	 * @param permNode
@@ -624,7 +624,7 @@ public class Utils {
 
 	/**
 	 * Broadcast message to every user since the bukkit one is bugged
-	 * 
+	 *
 	 * @param message
 	 */
 	public static void broadcastMessage(String message) {
@@ -703,7 +703,7 @@ public class Utils {
 
 	/**
 	 * Replace all the chosen material in the cuboid region.
-	 * 
+	 *
 	 * @param mat
 	 * @param block
 	 * @param radius
@@ -747,7 +747,7 @@ public class Utils {
 
 	/**
 	 * Broadcast a fakeQuit message for the selected player
-	 * 
+	 *
 	 * @param player
 	 *            that fake quit.
 	 */
@@ -763,7 +763,7 @@ public class Utils {
 
 	/**
 	 * Remove the player from the online list (TAB key)
-	 * 
+	 *
 	 * @param player
 	 *            player to remove
 	 */
@@ -781,7 +781,7 @@ public class Utils {
 
 	/**
 	 * Add the player in the online list (TAB key)
-	 * 
+	 *
 	 * @param player
 	 *            player to remove
 	 */
@@ -797,7 +797,7 @@ public class Utils {
 
 	/**
 	 * Broadcast a fakeJoin message for the selected player
-	 * 
+	 *
 	 * @param player
 	 *            that fake join.
 	 */
@@ -813,7 +813,7 @@ public class Utils {
 
 	/**
 	 * Because water and lava are fluid, using another algo to "delete"
-	 * 
+	 *
 	 * @param block
 	 * @param radius
 	 * @return
@@ -905,7 +905,7 @@ public class Utils {
 
 	/**
 	 * Get the elapsed time since the start.
-	 * 
+	 *
 	 * @param start
 	 * @return
 	 */
@@ -915,7 +915,7 @@ public class Utils {
 
 	/**
 	 * Transform a given time to an elapsed time.
-	 * 
+	 *
 	 * @param time
 	 *            in milisec
 	 * @return Long[] containing days, hours, mins and sec.
@@ -937,7 +937,7 @@ public class Utils {
 	/**
 	 * Replace the time and date to the format given in the config with the
 	 * corresponding date and time
-	 * 
+	 *
 	 * @author Lathanael
 	 * @param
 	 * @return timeFormatted
@@ -969,7 +969,7 @@ public class Utils {
 
 	/**
 	 * Get the real time from the server
-	 * 
+	 *
 	 * @author Lathanael
 	 * @param gmt
 	 *            The wanted GMT offset
@@ -986,7 +986,7 @@ public class Utils {
 
 	/**
 	 * Check if the block is a fluid.
-	 * 
+	 *
 	 * @param loc
 	 * @return
 	 */
@@ -1000,7 +1000,7 @@ public class Utils {
 
 	/**
 	 * Shortcut to online players.
-	 * 
+	 *
 	 * @return
 	 */
 	public static List<Player> getOnlinePlayers() {
@@ -1026,7 +1026,7 @@ public class Utils {
 	/**
 	 * Method to write a debug message in the log IF the debug mode is activated
 	 * in the configuration
-	 * 
+	 *
 	 * @param message
 	 *            message to write.
 	 */
@@ -1037,7 +1037,7 @@ public class Utils {
 
 	/**
 	 * Get the home by checking the colon
-	 * 
+	 *
 	 * @param sender
 	 *            who send the command
 	 * @param toParse
@@ -1075,7 +1075,7 @@ public class Utils {
 
 	/**
 	 * Get the prefix of the player, by checking the right the sender have
-	 * 
+	 *
 	 * @param player
 	 * @return
 	 */
@@ -1090,9 +1090,7 @@ public class Utils {
 			statusPrefix = Utils.I18n("invTitle");
 		if (AFKWorker.getInstance().isAfk(player))
 			statusPrefix = Utils.I18n("afkTitle") + statusPrefix;
-		if (mChatApi != null)
-			prefixstring = mChatApi.getPrefix(player);
-		prefixstring += PermissionManager.getPrefix(player);
+		prefixstring = PermissionManager.getPrefix(player);
 		String result = statusPrefix;
 		if (prefixstring.length() > 1)
 			result += prefixstring;
@@ -1130,7 +1128,7 @@ public class Utils {
 	/**
 	 * Check the if the player have the right to execute the command on the
 	 * other player
-	 * 
+	 *
 	 * @param sender
 	 *            the one who want to do the command
 	 * @param target
@@ -1160,7 +1158,7 @@ public class Utils {
 	/**
 	 * Check the if the player have the right to execute the command on the
 	 * other player
-	 * 
+	 *
 	 * @param sender
 	 *            the one who want to do the command
 	 * @param args
@@ -1226,7 +1224,7 @@ public class Utils {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see java.lang.Runnable#run()
 		 */
 		@Override
