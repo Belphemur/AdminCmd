6e5263c0fd7f9d9efe0d3f80466f0838a9916bd0
Balor <antoineaf@gmail.com>
2012-01-03 18:41:45 +0100

    Finish Teleport.

---
+++ src/main/java/be/Balor/Tools/Threads/TeleportTask.java
@@ -0,0 +1,46 @@
+/*This file is part of AdminCmd.
+
+    AdminCmd is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AdminCmd is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.*/
+package be.Balor.Tools.Threads;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+/**
+ * @author Balor (aka Antoine Aflalo)
+ *
+ */
+public class TeleportTask implements Runnable {
+	private final Player player;
+	private final Location loc;
+	
+	/**
+	 * @param player
+	 * @param loc
+	 */
+	public TeleportTask(Player player, Location loc) {
+		super();
+		this.player = player;
+		this.loc = loc;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		player.teleport(loc);
+	}
+
+}
