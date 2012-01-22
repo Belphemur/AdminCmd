3a944594f7e738a092c9db00392e5a2a938d3a56
Lathanael <101@abwesend.de>
2012-01-22 23:16:30 +0100

    Added version number to the Error Output. This should make it easier if ppl are to stupid to post the exact number and just say latest...

--- src/main/java/be/Balor/Manager/Commands/ACCommandContainer.java
+++ src/main/java/be/Balor/Manager/Commands/ACCommandContainer.java
@@ -1,16 +1,16 @@
 /************************************************************************
- * This file is part of AdminCmd.									
- *																		
+ * This file is part of AdminCmd.
+ *
  * AdminCmd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * AdminCmd is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdminCmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
@@ -24,7 +24,7 @@ import be.Balor.bukkit.AdminCmd.ACHelper;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
 public class ACCommandContainer {
 	private final CommandSender sender;
@@ -33,7 +33,7 @@ public class ACCommandContainer {
 	private final String[] argsStrings;
 
 	/**
- * 
+ *
  */
 	public ACCommandContainer(CommandSender sender, CoreCommand cmd, String[] args) {
 		this.sender = sender;
@@ -68,11 +68,13 @@ public class ACCommandContainer {
 
 	/**
 	 * Debug display
-	 * 
+	 *
 	 * @return
 	 */
 	public String debug() {
-		return "["
+		return "[Plugin Version: "
+				+ ACHelper.getInstance().getCoreInstance().getDescription().getVersion()
+				+ "]["
 				+ Thread.currentThread().getName()
 				+ "] The command "
 				+ cmd.getCmdName()
