e973ccb401a01be474e35b4f57e1a6be37a83c2e
Antoine Aflalo <antoineaf@gmail.com>
2012-02-02 19:15:51 +0100

    putting 0 as the default as the normal stop command.

--- src/main/java/be/Balor/bukkit/AdminCmd/ConfigEnum.java
+++ src/main/java/be/Balor/bukkit/AdminCmd/ConfigEnum.java
@@ -114,7 +114,7 @@ public enum ConfigEnum {
 	SCALE_TIMEOUT("timeOutScale", 60, "When using the parameter -t in power commands (god, fly, etc ...)\n"
 			+ "Configure the scale used for the time (here 60 sec).\n"
 			+ "It mean if you type /god -t 5 you will be god for 5 minutes"),
-	TIME_STOP("timeInSecBeforeStop", 5, "Time before stopping the server when using the AdminCmd command Stop.");
+	TIME_STOP("timeInSecBeforeStop", 0, "Time before stopping the server when using the AdminCmd command Stop.");
 
 	private final String confVal;
 	private final Object defaultVal;
