dd8907c892defa967e7491d77ee02a0bdbd0fc64
Antoine Aflalo <antoineaf@gmail.com>
2012-02-11 11:50:45 +0100

    License modification

--- src/main/java/be/Balor/World/SimpleLocation.java
+++ src/main/java/be/Balor/World/SimpleLocation.java
@@ -1,18 +1,18 @@
 /************************************************************************
- * This file is part of MySQLAddon.									
+ * This file is part of AdminCmd.									
  *																		
- * MySQLAddon is free software: you can redistribute it and/or modify
+ * AdminCmd is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by	
  * the Free Software Foundation, either version 3 of the License, or		
  * (at your option) any later version.									
  *																		
- * MySQLAddon is distributed in the hope that it will be useful,	
+ * AdminCmd is distributed in the hope that it will be useful,	
  * but WITHOUT ANY WARRANTY; without even the implied warranty of		
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
  * GNU General Public License for more details.							
  *																		
  * You should have received a copy of the GNU General Public License
- * along with MySQLAddon.  If not, see <http://www.gnu.org/licenses/>.
+ * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
 package be.Balor.World;
 
