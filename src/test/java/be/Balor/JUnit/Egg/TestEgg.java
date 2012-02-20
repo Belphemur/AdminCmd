54c9da53fab633eaa7d80388905de6c7f9bafd47
Lathanael <101@abwesend.de>
2012-02-20 17:18:42 +0100

    Un-/Suppressing warnings

--- src/test/java/be/Balor/JUnit/Egg/TestEgg.java
+++ src/test/java/be/Balor/JUnit/Egg/TestEgg.java
@@ -1,16 +1,16 @@
 /************************************************************************
- * This file is part of AdminCmd.									
- *																		
+ * This file is part of AdminCmd.
+ *
  * AdminCmd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * AdminCmd is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdminCmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
@@ -25,13 +25,14 @@ import be.Balor.Tools.Egg.Exceptions.ProcessingArgsException;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
+@SuppressWarnings("serial")
 public class TestEgg extends EggType<Boolean> {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see be.Balor.Tools.Egg.EggType#onEvent(org.bukkit.event.player.
 	 * PlayerEggThrowEvent)
 	 */
@@ -43,7 +44,7 @@ public class TestEgg extends EggType<Boolean> {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * be.Balor.Tools.Egg.EggType#processArguments(be.Balor.Manager.Commands
 	 * .CommandArgs)
@@ -59,7 +60,7 @@ public class TestEgg extends EggType<Boolean> {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see be.Balor.Tools.Egg.EggType#checkPermission(org.bukkit.entity.Player)
 	 */
 	@Override
