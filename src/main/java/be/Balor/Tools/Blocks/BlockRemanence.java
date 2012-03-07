406c925e7129f8795f128f74fe99e953d5765638
Lathanael <lathanael@admincmd.com>
2012-03-07 16:54:28 +0100

    Needs update!

--- src/main/java/be/Balor/Tools/Blocks/BlockRemanence.java
+++ src/main/java/be/Balor/Tools/Blocks/BlockRemanence.java
@@ -1,16 +1,16 @@
 /************************************************************************
- * This file is part of AdminCmd.									
- *																		
+ * This file is part of AdminCmd.
+ *
  * AdminCmd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * AdminCmd is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdminCmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
@@ -21,7 +21,7 @@ import org.bukkit.block.Block;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
 public class BlockRemanence {
 	protected Location loc;
@@ -30,7 +30,7 @@ public class BlockRemanence {
 	private boolean useData;
 
 	/**
-	 * 
+	 *
 	 */
 	public BlockRemanence(Location loc) {
 		this.loc = loc;
@@ -70,9 +70,10 @@ public class BlockRemanence {
 
 	/**
 	 * Block types. From worldEdit
-	 * 
+	 *
 	 * @author sk89q
 	 */
+	// TODO: Needs update
 	private static boolean usesData(int id) {
 		return id == 6 // Saplings
 				|| id == 8 // Water
