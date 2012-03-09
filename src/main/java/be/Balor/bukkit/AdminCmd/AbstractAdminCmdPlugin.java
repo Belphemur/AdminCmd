f23cfe09faae0e2bf1bdeb7a7bb92e3bb3892c23
Antoine Aflalo <antoineaf@gmail.com>
2012-03-09 01:18:51 +0100

    bug #9 still testing the hook.

--- src/main/java/be/Balor/bukkit/AdminCmd/AbstractAdminCmdPlugin.java
+++ src/main/java/be/Balor/bukkit/AdminCmd/AbstractAdminCmdPlugin.java
@@ -34,6 +34,7 @@ public abstract class AbstractAdminCmdPlugin extends JavaPlugin {
 	 * 
 	 * @param name
 	 *            the name used for the plugin.
+	 *            
 	 */
 	public AbstractAdminCmdPlugin(String name) {
 		this.name = name;
