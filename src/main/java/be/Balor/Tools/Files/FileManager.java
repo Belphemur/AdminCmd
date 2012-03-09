794ff5ed18b530226efffc3597b738d0cee69f1b
Lathanael <lathanael@admincmd.com>
2012-03-09 18:25:08 +0100

    Forgot to remove unused import

--- src/main/java/be/Balor/Tools/Files/FileManager.java
+++ src/main/java/be/Balor/Tools/Files/FileManager.java
@@ -28,7 +28,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.EnumMap;
