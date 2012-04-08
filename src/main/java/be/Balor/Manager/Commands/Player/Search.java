956fa380127a40c050f5588ec400c58dbb62c31e
Lathanael <lathanael@admincmd.com>
2012-04-08 13:30:08 +0200

    Added /search command. Addresses #24

---
+++ src/main/java/be/Balor/Manager/Commands/Player/Search.java
@@ -0,0 +1,85 @@
+/*************************************************************************
+ * This file is part of AdminCmd.
+ *
+ * AdminCmd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdminCmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdminCmd. If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
+
+package be.Balor.Manager.Commands.Player;
+
+import java.net.InetAddress;
+import java.util.List;
+import java.util.TreeSet;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.google.common.base.Joiner;
+
+import be.Balor.Manager.Commands.CommandArgs;
+import be.Balor.Manager.Exceptions.PlayerNotFound;
+import be.Balor.Player.ACPlayer;
+import be.Balor.Player.PlayerManager;
+import be.Balor.bukkit.AdminCmd.ACPluginManager;
+
+/**
+ * @author Lathanael (aka Philippe Leipold)
+ *
+ */
+public class Search extends PlayerCommand {
+
+	public Search() {
+		cmdName = "bal_search";
+		permNode = "admincmd.player.search";
+	}
+
+	/* (non-Javadoc)
+	 * @see be.Balor.Manager.Commands.CoreCommand#execute(org.bukkit.command.CommandSender, be.Balor.Manager.Commands.CommandArgs)
+	 */
+	@Override
+	public void execute(CommandSender sender, CommandArgs args) throws PlayerNotFound {
+		if (args.hasFlag('i')) {
+			final String ip = args.getValueFlag('i');
+			if (ip == null || ip.equals("") || ip.length() == 0)
+				return;
+			final Player[] onPlayers = ACPluginManager.getServer().getOnlinePlayers();
+			final List<ACPlayer> exPlayers = PlayerManager.getInstance().getExistingPlayers();
+			final TreeSet<String> players = new TreeSet<String>();
+			final String on = "[ON] ", off = "[OFF] ";
+			InetAddress ipAdress;
+			for (Player p : onPlayers) {
+				ipAdress = p.getAddress().getAddress();
+				if (ipAdress != null && ipAdress.toString().equals(ip))
+					players.add(on + p.getName());
+			}
+			String ip2;
+			for (ACPlayer p : exPlayers) {
+				ip2 = p.getInformation("last-ip").getString();
+				if (ip2 != null && ip2.toString().equals(ip))
+					players.add(off + p.getName());
+			}
+			String found = Joiner.on(", ").join(players);
+			sender.sendMessage(found);
+			return;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see be.Balor.Manager.Commands.CoreCommand#argsCheck(java.lang.String[])
+	 */
+	@Override
+	public boolean argsCheck(String... args) {
+		return args != null && args.length >= 1;
+	}
+}
