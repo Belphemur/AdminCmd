1b8d1b70e3e73cf9184401005da1b1a69943d277
Lathanael <lathanael@admincmd.com>
2012-04-12 22:33:00 +0200

    Fixed getting of IP address

--- src/main/java/be/Balor/Listeners/Commands/ACBanListener.java
+++ src/main/java/be/Balor/Listeners/Commands/ACBanListener.java
@@ -1,16 +1,16 @@
 /************************************************************************
- * This file is part of AdminCmd.									
- *																		
+ * This file is part of AdminCmd.
+ *
  * AdminCmd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * AdminCmd is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdminCmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
@@ -27,7 +27,7 @@ import be.Balor.bukkit.AdminCmd.ACHelper;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
 public class ACBanListener implements Listener {
 	@EventHandler(priority = EventPriority.LOWEST)
@@ -36,7 +36,7 @@ public class ACBanListener implements Listener {
 			return;
 		Ban player = ACHelper.getInstance().getBan(event.getPlayer().getName());
 		if (player == null)
-			player = ACHelper.getInstance().getBan(event.getAddress().toString().substring(1));
+			player = ACHelper.getInstance().getBan(event.getPlayer().getAddress().getAddress().toString().substring(1));
 		if (player != null)
 			event.disallow(Result.KICK_BANNED, player.getReason());
 	}
