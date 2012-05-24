9fb875f84154138c039eb73bc1472d819b70c6b7
Antoine Aflalo <antoineaf@gmail.com>
2012-05-24 19:29:06 +0200

    CopyRight from OpenInv for the concept and IDEA.

--- src/main/java/be/Balor/OpenInv/ACPlayerInventory.java
+++ src/main/java/be/Balor/OpenInv/ACPlayerInventory.java
@@ -1,19 +1,18 @@
-/************************************************************************
- * This file is part of AdminCmd.									
- *																		
- * AdminCmd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * AdminCmd is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+/**
+ * Copyright (C) 2011-2012 lishid. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
- * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
- ************************************************************************/
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package be.Balor.OpenInv;
 
 import net.minecraft.server.EntityHuman;
@@ -21,7 +20,7 @@ import net.minecraft.server.ItemStack;
 import net.minecraft.server.PlayerInventory;
 
 /**
- * @author Balor (aka Antoine Aflalo)
+ * @author lishd (Modded by Balor) {@link https://github.com/lishd/OpenInv/}
  * 
  */
 public class ACPlayerInventory extends PlayerInventory {
