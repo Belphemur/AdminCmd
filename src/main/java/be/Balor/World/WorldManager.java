999f2f7040a8b14749976a698f44b91c409bfd43
Antoine Aflalo <antoineaf@gmail.com>
2012-08-06 19:55:48 +0200

    Forgot to convert the warps in the WorldManager. See #230

--- src/main/java/be/Balor/World/WorldManager.java
+++ src/main/java/be/Balor/World/WorldManager.java
@@ -28,6 +28,7 @@ import org.bukkit.Location;
 import org.bukkit.World;
 
 import be.Balor.Manager.Exceptions.WorldNotLoaded;
+import be.Balor.Tools.Warp;
 import be.Balor.Tools.Debug.ACLogger;
 import be.Balor.Tools.Debug.DebugLog;
 import be.Balor.Tools.Help.String.Str;
@@ -104,6 +105,11 @@ public class WorldManager {
 			newWorld.setDifficulty(oldWorld.getDifficulty());
 			DebugLog.INSTANCE.info("Convert Default Spawn");
 			newWorld.setSpawn(oldWorld.getSpawn());
+			DebugLog.INSTANCE.info("Convert Warps");
+			for (final String warp : oldWorld.getWarpList()) {
+				final Warp w = oldWorld.getWarp(warp);
+				newWorld.addWarp(w.name, w.loc);
+			}
 			DebugLog.INSTANCE.info("Convert Informations");
 			for (final Entry<String, Object> info : oldWorld.getInformations()
 					.entrySet()) {
