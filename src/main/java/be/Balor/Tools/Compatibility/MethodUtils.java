1a0c99d3d38b1b9adb1183c28953dfdbe717e3bd
Antoine Aflalo <antoineaf@gmail.com>
2012-12-22 11:00:56 +0100

    [ADD] Method Utils.

---
+++ src/main/java/be/Balor/Tools/Compatibility/MethodUtils.java
@@ -0,0 +1,81 @@
+/************************************************************************
+ * This file is part of AdminCmd.									
+ *																		
+ * AdminCmd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by	
+ * the Free Software Foundation, either version 3 of the License, or		
+ * (at your option) any later version.									
+ *																		
+ * AdminCmd is distributed in the hope that it will be useful,	
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of		
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
+ * GNU General Public License for more details.							
+ *																		
+ * You should have received a copy of the GNU General Public License
+ * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+package be.Balor.Tools.Compatibility;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Antoine
+ * 
+ */
+public class MethodUtils {
+
+	/**
+	 * Get the method of the given class, even if it's a private method.
+	 * 
+	 * @param clazz
+	 *            - given class
+	 * @param name
+	 *            - name of the method
+	 * @param parameterTypes
+	 *            - parameter type of the method (Class<?>)
+	 * @return the wanted method if found
+	 * @throws RuntimeException
+	 *             if the method don't exist.
+	 */
+	public static Method getMethod(final Class<?> clazz, final String name,
+			final Class<?>... parameterTypes) {
+		Method method;
+		try {
+			method = clazz.getDeclaredMethod(name, parameterTypes);
+			return method;
+		} catch (final Exception e) {
+			throw new RuntimeException("Can't get method " + name + " from "
+					+ clazz, e);
+		}
+	}
+
+	/**
+	 * Invoke the method with the given argument on the given object. Works also
+	 * for private method.
+	 * 
+	 * @param obj
+	 *            - given object
+	 * @param method
+	 *            - given method
+	 * @param args
+	 *            - given arguments
+	 * @return the result of the method.
+	 * @throws RuntimeException
+	 *             if there is a problem while invoking the method.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T invokeMethod(final Object obj, final Method method,
+			final Object... args) {
+		T result = null;
+		method.setAccessible(true);
+		try {
+			result = (T) method.invoke(obj, args);
+		} catch (final Exception e) {
+			throw new RuntimeException("Can't invoke method " + method
+					+ " from " + obj, e);
+		} finally {
+			method.setAccessible(false);
+		}
+		return result;
+	}
+}
