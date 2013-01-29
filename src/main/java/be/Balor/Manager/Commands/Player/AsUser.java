21c1051fffacd8ab94971024c2588fbb37d012ec
Antoine Aflalo <antoineaf@gmail.com>
2013-01-28 22:28:07 -0500

    [ADD] Adding the /asuser command to launch a command as another user. See 364

---
+++ src/main/java/be/Balor/Manager/Commands/Player/AsUser.java
@@ -0,0 +1,89 @@
+/************************************************************************
+ * This file is part of AdminCmd.									
+ *																		
+ * AdminCmd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by	
+ * the Free Software Foundation, either version 3 of the License, or		
+ * (at your option) any later version.									
+ *																		
+ * AdminCmd is distributed in the hope that it will be useful,	
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of		
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
+ * GNU General Public License for more details.							
+ *																		
+ * You should have received a copy of the GNU General Public License
+ * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+package be.Balor.Manager.Commands.Player;
+
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import be.Balor.Manager.Commands.CommandArgs;
+import be.Balor.Manager.Exceptions.PlayerNotFound;
+import be.Balor.Manager.Permissions.ActionNotPermitedException;
+import be.Balor.Tools.Utils;
+import be.Balor.bukkit.AdminCmd.ACPluginManager;
+
+/**
+ * @author Antoine
+ * 
+ */
+public class AsUser extends PlayerCommand {
+
+	/**
+	 * 
+	 */
+	public AsUser() {
+		super("bal_asuser", "admincmd.player.asuser");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see be.Balor.Manager.Commands.CoreCommand#execute(org.bukkit.command.
+	 * CommandSender, be.Balor.Manager.Commands.CommandArgs)
+	 */
+	@Override
+	public void execute(final CommandSender sender, final CommandArgs args)
+			throws ActionNotPermitedException, PlayerNotFound {
+		final String playerName = args.getString(1);
+		final Player target = Bukkit.getPlayer(playerName);
+		if (target == null) {
+			final HashMap<String, String> replace = new HashMap<String, String>();
+			replace.put("player", playerName);
+			throw new PlayerNotFound(Utils.I18n("playerNotFound", replace),
+					sender);
+		}
+		if (!Utils.checkImmunity(sender, target)) {
+			throw new PlayerNotFound(Utils.I18n("insufficientLvl"), sender);
+		}
+		final StringBuffer argsBuffer = new StringBuffer();
+		for (int i = 1; i < args.length; i++) {
+			argsBuffer.append(args.getString(i)).append(" ");
+		}
+		ACPluginManager.scheduleSyncTask(new Runnable() {
+			@Override
+			public void run() {
+				Bukkit.getServer().dispatchCommand(target,
+						argsBuffer.toString().trim());
+
+			}
+		});
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see be.Balor.Manager.Commands.CoreCommand#argsCheck(java.lang.String[])
+	 */
+	@Override
+	public boolean argsCheck(final String... args) {
+		return args != null && args.length >= 2;
+	}
+
+}
