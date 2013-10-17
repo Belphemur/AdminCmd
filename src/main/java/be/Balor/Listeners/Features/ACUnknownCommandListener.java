bf5e0b21966b8446972d60e64c279cbe8d20269f
Antoine Aflalo <antoineaf@gmail.com>
2013-10-17 18:28:25 +0300

    [OPTI] Using FUZZY reflection to get commandMap.

--- src/main/java/be/Balor/Listeners/Features/ACUnknownCommandListener.java
+++ src/main/java/be/Balor/Listeners/Features/ACUnknownCommandListener.java
@@ -1,68 +1,67 @@
 package be.Balor.Listeners.Features;
 
-import be.Balor.bukkit.AdminCmd.ACPluginManager;
-import be.Balor.bukkit.AdminCmd.ConfigEnum;
-import be.Balor.bukkit.AdminCmd.LocaleHelper;
-import java.lang.reflect.Field;
 import org.bukkit.Server;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.plugin.SimplePluginManager;
+import org.bukkit.plugin.PluginManager;
 
-public class ACUnknownCommandListener
-        implements Listener {
+import be.Balor.Tools.Compatibility.Reflect.FieldUtils;
+import be.Balor.Tools.Compatibility.Reflect.Fuzzy.FuzzyFieldContract;
+import be.Balor.bukkit.AdminCmd.ACPluginManager;
+import be.Balor.bukkit.AdminCmd.ConfigEnum;
+import be.Balor.bukkit.AdminCmd.LocaleHelper;
 
-    private SimpleCommandMap cmdMap = null;
+public class ACUnknownCommandListener implements Listener {
 
-    public ACUnknownCommandListener() {
-        try {
-            this.cmdMap = getCommandMap();
-        } catch (Exception e) {
-        }
-    }
+	private SimpleCommandMap cmdMap = null;
 
-    private SimpleCommandMap getCommandMap() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException /*    */ {
-        Server svr = ACPluginManager.getServer();
-        if ((svr.getPluginManager() instanceof SimplePluginManager)) {
-            Field f = SimplePluginManager.class.getDeclaredField("commandMap");
-            f.setAccessible(true);
-            return (SimpleCommandMap) f.get(svr.getPluginManager());
-        }
-        return null;
-    }
+	public ACUnknownCommandListener() {
+		try {
+			this.cmdMap = getCommandMap();
+		} catch (final Exception e) {
+		}
+	}
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onUnknownCommand(PlayerCommandPreprocessEvent event) {
-        if (event.isCancelled()) {
-            return;
-        }
+	private SimpleCommandMap getCommandMap() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException /*    */{
+		final Server svr = ACPluginManager.getServer();
+		final FuzzyFieldContract contract = FuzzyFieldContract.newBuilder().declaringClassDerivedOf(PluginManager.class).typeDerivedOf(CommandMap.class)
+				.build();
+		return FieldUtils.getAttribute(svr.getPluginManager(), contract);
+	}
 
-        String cmd = event.getMessage();
-        if (cmd.charAt(0) == '/') {
-            cmd = cmd.replaceFirst("/", "");
-        }
-        cmd = cmd.split(" ")[0];
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onUnknownCommand(final PlayerCommandPreprocessEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
 
-        Player player = event.getPlayer();
-        if (!isCmdRegistered(cmd)) {
-            LocaleHelper.UNKNOWN_COMMAND.sendLocale(player);
-            System.out.println(player.getName() + " issued server command: " + event.getMessage());
-            event.setCancelled(true);
-        }
-    }
+		String cmd = event.getMessage();
+		if (cmd.charAt(0) == '/') {
+			cmd = cmd.replaceFirst("/", "");
+		}
+		cmd = cmd.split(" ")[0];
 
-    private boolean isCmdRegistered(String name) {
-        if (ConfigEnum.UNKNOWN_COMMAND_BYPASS.getStringList().contains(name)) {
-            return true;
-        }
+		final Player player = event.getPlayer();
+		if (!isCmdRegistered(cmd)) {
+			LocaleHelper.UNKNOWN_COMMAND.sendLocale(player);
+			System.out.println(player.getName() + " issued server command: " + event.getMessage());
+			event.setCancelled(true);
+		}
+	}
 
-        if (this.cmdMap == null) {
-            return true;
-        }
-        return this.cmdMap.getCommand(name) != null;
-    }
+	private boolean isCmdRegistered(final String name) {
+		if (ConfigEnum.UNKNOWN_COMMAND_BYPASS.getStringList().contains(name)) {
+			return true;
+		}
+
+		if (this.cmdMap == null) {
+			return true;
+		}
+		return this.cmdMap.getCommand(name) != null;
+	}
 }
