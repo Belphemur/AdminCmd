4a17d974e83c65fbd337870e0ad83bd7f91e57d7
TheJeterLP <joey.peter1998@gmail.com>
2014-03-30 16:45:30 +0200

    [MOD] Modifiy way how single plugins are reloaded. See #524

--- src/main/java/be/Balor/Manager/Commands/Server/Reload.java
+++ src/main/java/be/Balor/Manager/Commands/Server/Reload.java
@@ -1,19 +1,19 @@
-/************************************************************************
- * This file is part of AdminCmd.									
- *																		
+/** **********************************************************************
+ * This file is part of AdminCmd.
+ *
  * AdminCmd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * AdminCmd is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdminCmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
- * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
- ************************************************************************/
+ * along with AdminCmd. If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************** */
 package be.Balor.Manager.Commands.Server;
 
 import org.bukkit.command.CommandSender;
@@ -23,67 +23,77 @@ import be.Balor.Manager.LocaleManager;
 import be.Balor.Manager.Commands.CommandArgs;
 import be.Balor.Manager.Exceptions.ActionNotPermitedException;
 import be.Balor.Manager.Exceptions.PlayerNotFound;
+import be.Balor.Tools.FileUpdate;
 import be.Balor.bukkit.AdminCmd.ACHelper;
 import be.Balor.bukkit.AdminCmd.ACPluginManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
 public class Reload extends ServerCommand {
 
-	/**
-	 * 
-	 */
-	public Reload() {
-		permNode = "admincmd.server.reload";
-		cmdName = "bal_reload";
-	}
+        /**
+         *
+         */
+        public Reload() {
+                permNode = "admincmd.server.reload";
+                cmdName = "bal_reload";
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * be.Balor.Manager.ACCommands#execute(org.bukkit.command.CommandSender,
-	 * java.lang.String[])
-	 */
-	@Override
-	public void execute(final CommandSender sender, final CommandArgs args)
-			throws ActionNotPermitedException, PlayerNotFound {
-		if (args.length >= 1 && !args.getString(0).equals("AdminCmd")) {
-			final Plugin plugin = sender.getServer().getPluginManager()
-					.getPlugin(args.getString(0));
-			if (plugin == null) {
-				LocaleManager.sI18n(sender, "pluginNotFound", "plugin",
-						args.getString(0));
-				return;
-			}
-			ACPluginManager.scheduleSyncTask(new Runnable() {
-				@Override
-				public void run() {
-					ACPluginManager.getServer().getPluginManager()
-							.disablePlugin(plugin);
-					ACPluginManager.getServer().getPluginManager()
-							.enablePlugin(plugin);
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * be.Balor.Manager.ACCommands#execute(org.bukkit.command.CommandSender,
+         * java.lang.String[])
+         */
+        @Override
+        public void execute(final CommandSender sender, final CommandArgs args)
+                        throws ActionNotPermitedException, PlayerNotFound {
+                if (args.length >= 1 && !args.getString(0).equals("AdminCmd")) {
+                        final Plugin plugin = sender.getServer().getPluginManager()
+                                        .getPlugin(args.getString(0));
+                        final JavaPlugin javaPlugin = (JavaPlugin) plugin;
+                        if (plugin == null) {
+                                LocaleManager.sI18n(sender, "pluginNotFound", "plugin",
+                                                args.getString(0));
+                                return;
+                        }
+                        ACPluginManager.scheduleSyncTask(new Runnable() {
+                                @Override
+                                public void run() {
+                                        try {
+                                                FileUpdate.reload(javaPlugin);
+                                                /* ACPluginManager.getServer().getPluginManager()
+                                                 * .disablePlugin(plugin);
+                                                 * ACPluginManager.getServer().getPluginManager()
+                                                 * .enablePlugin(plugin); */
+                                        } catch (Exception ex) {
+                                                ex.printStackTrace();
+                                        }
 
-				}
-			});
-			LocaleManager.sI18n(sender, "pluginReloaded", "plugin", args.getString(0));
-		} else {
-			ACHelper.getInstance().reload();
-			LocaleManager.sI18n(sender, "pluginReloaded", "plugin", "AdminCmd");
-		}
+                                }
+                        });
+                        LocaleManager.sI18n(sender, "pluginReloaded", "plugin", args.getString(0));
+                } else {
+                        ACHelper.getInstance().reload();
+                        LocaleManager.sI18n(sender, "pluginReloaded", "plugin", "AdminCmd");
+                }
 
-	}
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see be.Balor.Manager.ACCommands#argsCheck(java.lang.String[])
-	 */
-	@Override
-	public boolean argsCheck(final String... args) {
-		return args != null;
-	}
+        /*
+         * (non-Javadoc)
+         *
+         * @see be.Balor.Manager.ACCommands#argsCheck(java.lang.String[])
+         */
+        @Override
+        public boolean argsCheck(final String... args) {
+                return args != null;
+        }
 
 }
---
+++ src/main/java/be/Balor/Tools/FileUpdate.java
@@ -0,0 +1,79 @@
+package be.Balor.Tools;
+
+import be.Balor.bukkit.AdminCmd.ACPluginManager;
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.bukkit.command.Command;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.SimpleCommandMap;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.SimplePluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FileUpdate {
+
+        public static void reload(JavaPlugin pl) throws Exception {
+                File plFile = new File(pl.getClass().getProtectionDomain().getCodeSource().getLocation().getFile().replaceAll("%20", " "));
+                PluginManager manager = ACPluginManager.getServer().getPluginManager();
+                SimplePluginManager spmanager = (SimplePluginManager) manager;               
+                manager.disablePlugin(pl);
+                Field pluginsField = spmanager.getClass().getDeclaredField("plugins");
+                pluginsField.setAccessible(true);
+                List<Plugin> plugins = (List<Plugin>) pluginsField.get(spmanager);
+
+                Field lookupNamesField = spmanager.getClass().getDeclaredField("lookupNames");
+                lookupNamesField.setAccessible(true);
+                Map<String, Plugin> lookupNames = (Map<String, Plugin>) lookupNamesField.get(spmanager);
+
+                Field commandMapField = spmanager.getClass().getDeclaredField("commandMap");
+                commandMapField.setAccessible(true);
+                SimpleCommandMap commandMap = (SimpleCommandMap) commandMapField.get(spmanager);
+
+                Field knownCommandsField = null;
+                Map<String, Command> knownCommands = null;
+
+                if (commandMap != null) {
+                        knownCommandsField = commandMap.getClass().getDeclaredField("knownCommands");
+                        knownCommandsField.setAccessible(true);
+                        knownCommands = (Map<String, Command>) knownCommandsField.get(commandMap);
+                }
+
+                for (Plugin plugin : manager.getPlugins()) {
+                        if (plugin.getDescription().getName().equalsIgnoreCase(pl.getName())) {
+                                manager.disablePlugin(plugin);
+
+                                if (plugins != null && plugins.contains(plugin)) {
+                                        plugins.remove(plugin);
+                                }
+
+                                if (lookupNames != null && lookupNames.containsKey(pl.getDescription().getName())) {
+                                        lookupNames.remove(pl.getDescription().getName());
+                                }
+
+                                if (commandMap != null) {
+                                        for (Iterator<Map.Entry<String, Command>> it = knownCommands.entrySet().iterator(); it.hasNext();) {
+                                                Map.Entry<String, Command> entry = it.next();
+
+                                                if (entry.getValue() instanceof PluginCommand) {
+                                                        PluginCommand command = (PluginCommand) entry.getValue();
+
+                                                        if (command.getPlugin() == plugin) {
+                                                                command.unregister(commandMap);
+                                                                it.remove();
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
+                }
+
+                Plugin pluginload = manager.loadPlugin(plFile);
+                pluginload.onLoad();
+                manager.enablePlugin(pluginload);
+        }
+
+}
