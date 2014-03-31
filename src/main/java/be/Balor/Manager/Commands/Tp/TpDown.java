44448e63ebc89089c5de01a85911db4b051eb10f
TheJeterLP <joey.peter1998@gmail.com>
2014-03-31 15:24:04 +0200

    [MOD] Add way to teleport through air.

--- src/main/java/be/Balor/Manager/Commands/Tp/TpDown.java
+++ src/main/java/be/Balor/Manager/Commands/Tp/TpDown.java
@@ -1,4 +1,4 @@
-/************************************************************************
+/** **********************************************************************
  * This file is part of AdminCmd.
  *
  * AdminCmd is free software: you can redistribute it and/or modify
@@ -8,12 +8,12 @@
  *
  * AdminCmd is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
- ************************************************************************/
+ * along with AdminCmd. If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************** */
 package be.Balor.Manager.Commands.Tp;
 
 import be.Balor.Manager.Commands.CommandArgs;
@@ -25,6 +25,7 @@ import be.Balor.bukkit.AdminCmd.ACPluginManager;
 import be.Balor.bukkit.AdminCmd.LocaleHelper;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -42,20 +43,40 @@ public class TpDown extends TeleportCommand {
         public void execute(CommandSender sender, CommandArgs args) throws ActionNotPermitedException, PlayerNotFound {
                 if (!Users.isPlayer(sender)) return;
                 final Player player = Users.getUserParam(sender, args, permNode);
-                
+
                 Location loc = player.getLocation();
                 Location target = loc.clone();
                 Location target2 = loc.clone();
 
                 boolean found = false;
 
-                for (int y = loc.getBlockY() - 2; y >= 0; y--) {
-                        target.setY(y);
-                        target2.setY(y + 1);
-                        
-                        if (target.getBlock().getType() == Material.AIR && target2.getBlock().getType() == Material.AIR) {
-                                found = true;                               
-                                break;
+                Location underFeet = loc.getBlock().getRelative(BlockFace.DOWN, 2).getLocation();
+
+                boolean air = (underFeet.getBlock().getType() == Material.AIR);
+
+                if (air) {
+                        for (int y = underFeet.getBlockY() - 1; y >= 0; y--) {
+                                target.setY(y);
+                                target2.setY(y + 1);
+                                Location target3 = target2.clone();
+                                target3.setY(y + 2);
+
+                                if (target.getBlock().getType() != Material.AIR && target2.getBlock().getType() == Material.AIR && target3.getBlock().getType() == Material.AIR) {
+                                        found = true;
+                                        target.setY(y + 1);
+                                        break;
+                                }
+                        }
+                } else {
+
+                        for (int y = loc.getBlockY() - 2; y >= 0; y--) {
+                                target.setY(y);
+                                target2.setY(y + 1);
+
+                                if (target.getBlock().getType() == Material.AIR && target2.getBlock().getType() == Material.AIR) {
+                                        found = true;
+                                        break;
+                                }
                         }
                 }
 
--- src/main/java/be/Balor/Manager/Commands/Tp/TpUp.java
+++ src/main/java/be/Balor/Manager/Commands/Tp/TpUp.java
@@ -1,4 +1,4 @@
-/************************************************************************
+/** **********************************************************************
  * This file is part of AdminCmd.
  *
  * AdminCmd is free software: you can redistribute it and/or modify
@@ -8,12 +8,12 @@
  *
  * AdminCmd is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
- ************************************************************************/
+ * along with AdminCmd. If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************** */
 package be.Balor.Manager.Commands.Tp;
 
 import be.Balor.Manager.Commands.CommandArgs;
@@ -42,20 +42,39 @@ public class TpUp extends TeleportCommand {
         public void execute(CommandSender sender, CommandArgs args) throws ActionNotPermitedException, PlayerNotFound {
                 if (!Users.isPlayer(sender)) return;
                 final Player player = Users.getUserParam(sender, args, permNode);
-                
+
                 Location loc = player.getLocation();
                 Location target = loc.clone();
                 Location target2 = loc.clone();
 
                 boolean found = false;
 
-                for (int y = loc.getBlockY() + 2; y <= loc.getWorld().getMaxHeight(); y++) {
-                        target.setY(y);
-                        target2.setY(y + 1);
-                        
-                        if (target.getBlock().getType() == Material.AIR && target2.getBlock().getType() == Material.AIR) {
-                                found = true;                               
-                                break;
+                Location overHead = loc.clone();
+                overHead.setY(overHead.getBlockY() + 2);
+
+                boolean air = (overHead.getBlock().getType() == Material.AIR);
+
+                if (air) {
+                        for (int y = overHead.getBlockY(); y <= loc.getWorld().getMaxHeight(); y++) {
+                                target.setY(y + 1);
+                                target2.setY(y + 2);
+                                Location target3 = target2.clone();
+                                target3.setY(y + 3);
+                                if (target.getBlock().getType() != Material.AIR && target2.getBlock().getType() == Material.AIR && target3.getBlock().getType() == Material.AIR) {
+                                        found = true;
+                                        target.setY(y + 2);
+                                        break;
+                                }
+                        }
+                } else {
+                        for (int y = overHead.getBlockY(); y <= loc.getWorld().getMaxHeight(); y++) {
+                                target.setY(y);
+                                target2.setY(y + 1);
+
+                                if (target.getBlock().getType() == Material.AIR && target2.getBlock().getType() == Material.AIR) {
+                                        found = true;                                      
+                                        break;
+                                }
                         }
                 }
 
